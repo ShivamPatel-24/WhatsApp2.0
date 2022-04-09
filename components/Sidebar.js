6b6527c0bdcd7ff9ee9169e76c208b82cae31eb7
Shivam Patel <shivampatel@Shivams-Air.attlocal.net>
2022-04-09 00:14:19 -0400

    initial commit

---
+++ components/Sidebar.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import {Avatar, IconButton, Button} from '@material-ui/core/'
+import Chat from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Sidebar(props) {
+    return (
+        <Container>
+            <Header>
+                <UserAvatar />
+                    <IconContainer>
+                        <IconButton>
+                            <Chat />
+                        </IconButton>
+                        <IconButton>
+                            <MoreVertIcon />
+                        </IconButton>
+                        
+                    </IconContainer>
+            </Header>
+
+            <Search>
+                <SearchIcon />
+                <SearchInput placeholder="SEARCH IN CHATS"/>
+            </Search>
+
+            <SearchbarButton>Start a new chat</SearchbarButton>
+        </Container>
+    );
+}
+
+export default Sidebar;
+
+const Container = styled.div``; 
+
+const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    background-color: #fff;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    padding: 15px;
+    border-bottom: 1px solid whitesmoke;
+`; 
+
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover{
+        opacity: 0.8;
+    }
+`;
+
+const IconContainer = styled.div``;
+
+const Search = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+    flex: 1;
+    border: none;
+    outline-width: 0;
+    font-size: 20px;
+`;
+
+const SearchbarButton = styled(Button)`
+    width: 100%;
+    font-Size: 20px;
+    
+    &&&{
+        border-top: 1px solid whitesmoke;
+        border-bottom: 1px solid whitesmoke;
+    }
+`;
